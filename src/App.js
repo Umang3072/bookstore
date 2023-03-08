a7b438d7c7d62b8aaf50fea9675c4ca273e069a5
Umang Savaliya <umangsavaliya3072@gmail.com>
2023-03-09 00:43:41 +0530

    add styling and css basics
    \

--- src/App.js
+++ src/App.js
@@ -11,40 +11,45 @@ import React from 'react'
 // import ParentComponent from './pages/ParentComponent'
 // import UserGreeting from './pages/UserGreeting'
 // import NameList from './component/NameList'
-import About from './pages/About'
-import Title from './component/Title'
-import List from './component/List'
+// import About from './pages/About'
+// import Title from './component/Title'
+// import List from './component/List'
+// import Stylesheet from './component/Stylesheet'
+// import Inline from './component/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
+
 class App extends React.Component {
-  constructor(props) {
-    super(props)
+  // constructor(props) {
+  //   super(props)
   
-    this.state = {
-       isAbout : false,
-       isTitle : false
-    }
-  }
+  //   this.state = {
+  //      isAbout : false,
+  //      isTitle : false
+  //   }
+  // }
 
-  clickAbout(){
-      this.setState({
-        isAbout : true,
-        isTitle : false
-      })
-  }
+  // clickAbout(){
+  //     this.setState({
+  //       isAbout : true,
+  //       isTitle : false
+  //     })
+  // }
 
-  clickTitle(){
-    this.setState({
-      isTitle : true,
-      isAbout : false
-    })
-  }
+  // clickTitle(){
+  //   this.setState({
+  //     isTitle : true,
+  //     isAbout : false
+  //   })
+  // }
   
   render(){
-    let page
-    if (this.state.isAbout){
-      page = <About/>
-    } else {
-      page = <Title/>
-    }
+    // let page
+    // if (this.state.isAbout){
+    //   page = <About/>
+    // } else {
+    //   page = <Title/>
+    // }
   return (
     <div className="App">
       {/* <Greet name='Umang'>
@@ -78,7 +83,18 @@ class App extends React.Component {
       {/* <button onClick={() => this.clickAbout()}>About</button>
       <button onClick={() => this.clickTitle()}>Title</button>
       {page} */}
-      <List/>
+
+      {/* <List/> */}
+
+      {/* <Stylesheet/> */}
+
+      {/* <Stylesheet primary={true}/> */}
+
+      {/* <Inline/> */}
+
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+
     </div>
   );
   }
